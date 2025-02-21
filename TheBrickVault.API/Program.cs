f115f37b93cdec40064e6795bc12c19cd0a61a94
Jena L.R. White <jenale@gmail.com>
2025-02-21 13:21:51 -0500

    packages installed and references put in place but with errors

--- TheBrickVault.API/Program.cs
+++ TheBrickVault.API/Program.cs
@@ -1,16 +1,12 @@
-
-
-
 using TheBrickVault.API;
-
-
+using Microsoft.EntityFrameworkCore;
+using TheBrickVault.Infrastructure.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
+builder.Services.AddDbContext<LegoDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddHttpClient("RebrickableClient", client =>
 {
@@ -32,28 +28,7 @@ builder.Services.Configure<RebrickableSettings>(config.GetSection("Rebrickable")
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
