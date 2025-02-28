2c8563cb4d8cc72860b1247218d7c33879b408c9
Jena L.R. White <jenale@gmail.com>
2025-02-27 23:40:21 -0500

    moved TheBrickVault to a Blazor Web App and redid the repo

---
+++ Core/Models/LegoSet.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBrickVault.Core.Models
+{
+    public class LegoSet
+    {
+        public int Id { get; set; } //might delete this if SetNumber is sufficient. Or use this to number/list the current sets the user has. 
+        public string Name { get; set; }
+        //do SetNumber first and only to see the Db build successfully then add the rest. 
+        public int SetNum { get; set; }
+        public string Images { get; set; }
+        public int PieceCount { get; set; }
+
+        public int Instructions { get; set; } //might need to change type to string.
+        public int PartsList { get; set; } //not sure if type is int or string. 
+
+    }
+}
---
+++ Services/RebrickableService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.Json;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TheBrickVault.Core.Models;
+using TheBrickVault.Services;
+
+
+namespace TheBrickVault.Services
+{
+    public class RebrickableService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly RebrickableSettings _rebrickableSettings;
+
+        public RebrickableService(IHttpClientFactory clientFactory, RebrickableSettings rebrickableSettings)
+        {
+            _clientFactory = clientFactory;
+            _rebrickableSettings = rebrickableSettings;
+        }
+        public async Task<LegoSet> GetSetDetailsAsync(int setNum)
+        {
+            var client = _clientFactory.CreateClient("RebrickableClient");
+            var response = await client.GetAsync($"lego/sets/{setNum}/");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<LegoSet>(content);
+
+            }
+            return null;
+        }
+
+    }
+}
