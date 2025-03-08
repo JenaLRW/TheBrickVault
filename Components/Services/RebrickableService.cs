bcd6dadcc58dd1091c1550844589abd7e044fc03
Jena L.R. White <jenale@gmail.com>
2025-03-08 15:35:39 -0500

    changing how i want the app to run, some files cleared out for new codes

--- Components/Services/RebrickableService.cs
+++ Components/Services/RebrickableService.cs
@@ -8,30 +8,51 @@ using TheBrickVault.Core.Models;
 
 namespace TheBrickVault.Components.Services
 {
+    //The comments are really just for me to remind myself the purpose of the class and how it fits into 
+    //the overall architecture of the application.
+
+    //This class is the first step in the process of user searching for lego sets. 
+
+    //RebrickableService is to handle fetching data from the Rebrickable API and processing it into a 
+    //format that can be used by the application.
+
+    //Use the HttpClient to connect to Rebrickable's API endpoints and search for Lego sets.
+
+    //The RebrickableService will be responsible for making the API calls, handling the responses, and 
+    //converting the data into a format that can be used by the application.
     public class RebrickableService
     {
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly RebrickableSettings _rebrickableSettings;
-
-        public RebrickableService(IHttpClientFactory clientFactory, RebrickableSettings rebrickableSettings)
-        {
-            _clientFactory = clientFactory;
-            _rebrickableSettings = rebrickableSettings;
-        }
-        public async Task<LegoSet> GetSetDetailsAsync(int setNum)
-        {
-            var client = _clientFactory.CreateClient("RebrickableClient");
-            var response = await client.GetAsync($"lego/sets/{setNum}/");
-
-            if (response.IsSuccessStatusCode)
-            {
-
-                var content = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<LegoSet>(content);
-
-            }
-            return null;
-        }
+        private readonly HttpClient _httpClient; //IHttpClientFactory _clientFactory; is for more complex scenarios and I am not doing that here. 
+        private readonly string _apiKey; 
 
     }
+
+    // old design.  trying a new design and keeping old design in case the new is a fail.
+
+    //public class RebrickableService
+    //{
+    //    private readonly IHttpClientFactory _clientFactory;
+    //    private readonly RebrickableSettings _rebrickableSettings;
+
+    //    public RebrickableService(IHttpClientFactory clientFactory, RebrickableSettings rebrickableSettings)
+    //    {
+    //        _clientFactory = clientFactory;
+    //        _rebrickableSettings = rebrickableSettings;
+    //    }
+    //    public async Task<LegoSet> GetSetDetailsAsync(int setNum)
+    //    {
+    //        var client = _clientFactory.CreateClient("RebrickableClient");
+    //        var response = await client.GetAsync($"lego/sets/{setNum}/");
+
+    //        if (response.IsSuccessStatusCode)
+    //        {
+
+    //            var content = await response.Content.ReadAsStringAsync();
+    //            return JsonConvert.DeserializeObject<LegoSet>(content);
+
+    //        }
+    //        return null;
+    //    }
+
+    //}
 }
--- Infrastructure/DbInitializer.cs
+++ Infrastructure/DbInitializer.cs
@@ -12,7 +12,8 @@ namespace TheBrickVault.Infrastructure
                 return;
 
             var legoSets = new List<LegoSet>
-            {  
+
+            {
             new LegoSet { Id = 1, Name = "Harry Potter", SetNum = "78945", PieceCount = 4654 },
             new LegoSet { Id = 2, Name = "Star Wars", SetNum = "457245", PieceCount = 244 }
 
