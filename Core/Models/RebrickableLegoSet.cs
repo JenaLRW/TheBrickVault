b102e69ef3ff5981245b0869d5536cf963c244cd
Jena L.R. White <jenale@gmail.com>
2025-03-08 23:23:38 -0500

    Search Lego Set added to Blazor's page, no magic yet

---
+++ Core/Models/RebrickableLegoSet.cs
@@ -0,0 +1,11 @@
+﻿namespace TheBrickVault.Core.Models
+{
+    public class RebrickableLegoSet
+    {
+        public string? set_num { get; set; }
+        public string? name { get; set; }
+        public int? year { get; set; }
+        public int? num_parts { get; set; }
+        public string? set_img_url { get; set; }
+    }
+}
