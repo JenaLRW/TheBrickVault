2e9f4441cfd3fd6cc32b0bf12101df703545d8e6
Jena L.R. White <jenale@gmail.com>
2025-03-28 15:35:48 -0400

    Using imported data instead to find matching sets. Few accessories added.  No code yet

---
+++ Core/Models/Imported_Sets.cs
@@ -0,0 +1,15 @@
+﻿namespace TheBrickVault.Core.Models
+{
+    public class Imported_Sets
+    {
+        public int set_num { get; set; }
+        public string? name { get; set; }
+        public int? year { get; set; }
+        public string? theme_id { get; set; }
+
+        public string? num_parts { get; set; }
+
+        public string? img_url { get; set; }
+
+    }
+}
---
+++ Core/Models/Imported_inventory_sets.cs
@@ -0,0 +1,10 @@
+﻿namespace TheBrickVault.Core.Models
+{
+    public class Imported_inventory_sets
+    {
+        public int inventory_id { get; set; } 
+
+        public int set_num { get; set; } 
+
+        public int? quantity { get; set; } 
+    }
